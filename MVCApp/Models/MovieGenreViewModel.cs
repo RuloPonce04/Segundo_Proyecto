44a58f454f93c00ef31c578bf5d0230082467aa7
RuloPonce04 <raulponcelopez2002@gmail.com>
2023-11-19 22:40:52 -0600

    Fisrt Commit

---
+++ MVCApp/Models/MovieGenreViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCApp.Controllers;
+using System.Collections.Generic;
+
+
+namespace MVCApp.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        public string? MovieGenre {  get; set; }
+
+        public string? SearchString {  get; set; }
+
+    }
+}
